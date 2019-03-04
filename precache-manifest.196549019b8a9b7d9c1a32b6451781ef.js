399bd2b591d7a94f1678d2a6b08059349452701a
Skr <oscaraparisisancho@gmail.com>
2019-03-04 17:36:31 +0100

    Updates

---
+++ precache-manifest.196549019b8a9b7d9c1a32b6451781ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddd0172a80f745c9f4fd",
+    "url": "/practicaEcommerce/static/css/main.2998e93c.chunk.css"
+  },
+  {
+    "revision": "ddd0172a80f745c9f4fd",
+    "url": "/practicaEcommerce/static/js/main.ddd0172a.chunk.js"
+  },
+  {
+    "revision": "f13a5bd91931373c504b",
+    "url": "/practicaEcommerce/static/js/runtime~main.f13a5bd9.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/practicaEcommerce/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "f665838525789953fc2596e6853a5f2d",
+    "url": "/practicaEcommerce/index.html"
+  }
+];
