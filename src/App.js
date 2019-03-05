ff21fc5a4abce4a346ad3b3f4dc180121cca94a5
Skr <oscaraparisisancho@gmail.com>
2019-03-05 01:24:58 +0100

    Diseño final del panel login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logoCecotec from './img/cecotec-logo.png';
 import correo from './img/email.png';
 import candado from './img/candado.png';
 
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -11,34 +12,46 @@ class App extends Component {
       'panel': "login",
     }
   }
+
   render() {
     return (
       <div className="App">
-        {
-        this.state.panel === "login" &&
-          <div className="panel-login">
-            <div className="panel-login-1">
-              <img className="logoM" src={logoCecotec}/>
-              <div>
-                <img className="icono" src={correo}/>
-                <input placeholder="Correo" />
-              </div>
-              <div>
-                <img className="icono" src={candado}/>
-                <input placeholder="Contraseña" />
-              </div>
-              <button className="boton-nuevaCuenta botonSinEstilo">Nueva cuenta</button>
-
-            </div>
-            <div className="panel-login-2">
-              <h1>Bienvenido!</h1>
-              <p>Binvenido al panel de gestión de tu Eccomerce. Por favor, introduce tus datos de usuario o crea una nueva cuenta.</p>
-            </div>
-
-          </div>
-        }
+        { this.state.panel === "login" && <this.PanelLogin /> }
       </div>
     );
+  }
+
+
+  PanelLogin(){
+    let validate = ()=>{
+      var mail = $('#mail').val();
+      var pass = $('#pass').val();
+    }
+    return(
+      <form className="panel-login" onSubmit={validate()}>
+        <div className="panel-login-1">
+          <img className="logoM" src={logoCecotec}/>
+          <div className="contenedor-input">
+            <img className="icono" src={correo}/>
+            <input id="mail" type="email" placeholder="Correo" />
+            <p className="mailErr"/>
+          </div>
+          <div className="contenedor-input">
+            <img className="icono" src={candado}/>
+            <input id="pass" type="text" placeholder="Contraseña" />
+            <p className="passErr"/>
+          </div>
+          <div className="contenedor-boton">
+            <button className="boton-enviar" type="submit">Enviar</button>
+            <button className="boton-nuevaCuenta botonSinEstilo">Nueva cuenta</button>
+          </div>
+        </div>
+        <div className="panel-login-2">
+          <h1>Bienvenido!</h1>
+          <p>Binvenido al panel de gestión de tu Eccomerce. Por favor, introduce tus datos de usuario o crea una nueva cuenta.</p>
+        </div>
+      </form>
+    )
   }
 }
 
